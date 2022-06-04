e50caa3e2b2191771d6f629fc0e72a4cddffc87a
wi21b025 <wi21b025@technikum-wien.at>
2022-06-04 17:04:05 +0200

    Initial commit please

---
+++ src/main/java/com/example/slm_tesr/SlmTesrApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_tesr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTesrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTesrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm_tesr/SlmTesrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_tesr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTesrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
