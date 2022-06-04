c1e77ffbc1f3f8cb1b4a7828fcf32e8e77166bfb
wi21b025 <wi21b025@technikum-wien.at>
2022-06-04 17:16:42 +0200

    Initial Commit Sum

---
+++ src/main/java/com/example/slm_tesr/CalculatorSLM.java
@@ -0,0 +1,13 @@
+package com.example.slm_tesr;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorSLM {
+    @RequestMapping("/sum")
+    public int sum(@RequestParam int a,@RequestParam int b){
+        return a + b;
+    }
+}
